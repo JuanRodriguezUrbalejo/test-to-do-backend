26b575d2661d41edbf0b38b5432e11e9297ea5aa
JuanRodriguezUrbalejo <95185759+JuanRodriguezUrbalejo@users.noreply.github.com>
2024-08-16 21:46:49 -0700

    creacion de aplicacion tareas
    Se creo la aplicación de tareas.
    Se creo el modelo de tareas y listas.

--- test_to_do_backend/test_to_do_backend/settings.py
+++ test_to_do_backend/test_to_do_backend/settings.py
@@ -25,8 +25,20 @@ SECRET_KEY = 'django-insecure-9v6mo9(ecy=j#8z*_1$!c=5d+0+(tcc^ky*#m_=i9h2t#tvmhx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = [
+    'Access-Control-Allow-Origin',
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',     
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with'
+]
 
 # Application definition
 
@@ -37,6 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'to_do',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'test_to_do_backend.urls'
