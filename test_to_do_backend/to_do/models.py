7de31edc270882e19a1f4a76c42c539ac63477a0
JuanRodriguezUrbalejo <95185759+JuanRodriguezUrbalejo@users.noreply.github.com>
2024-08-22 18:11:40 -0700

    cambios al backend
    se agrego el poder retornar una lista con todas sus tareas asociadas mediante un id

--- test_to_do_backend/to_do/models.py
+++ test_to_do_backend/to_do/models.py
@@ -14,7 +14,7 @@ class Tasks(models.Model):
     name = models.CharField(max_length = 100, verbose_name = 'Nombre de tarea')
     start_date = models.DateTimeField(blank = True, null = True, verbose_name = 'Fecha de inicio')
     list = models.ForeignKey(Lists, on_delete = models.CASCADE, verbose_name = 'Lista')
-    priority = models.CharField(max_length = 1, choices = (('H','Alta'), ('M', 'Media'), ('L', 'Baja')))
+    priority = models.CharField(max_length = 1, choices = (('H','Alta'), ('M', 'Media'), ('L', 'Baja')), verbose_name = 'Prioridad')
     status = models.BooleanField(default = False, verbose_name = 'Estado') #false es pendiente y true completado
     is_active = models.BooleanField(default = True, verbose_name = 'Activo')
     
--- test_to_do_backend/to_do/serializer.py
+++ test_to_do_backend/to_do/serializer.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from .models import Tasks, Lists
 
-class ListsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lists
-        fields = '__all__'
 
 class TasksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tasks
         fields = '__all__'
+
+class ListsSerializer(serializers.ModelSerializer):
+    tasks = TasksSerializer(many=True, read_only=True, source='tasks_set')
+
+    class Meta:
+        model = Lists
+        fields = '__all__'
+
